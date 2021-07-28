5562b4eaf56db0dd21c6180de69cae3314c6838b
Leandro Basilio <leandroab07@gmail.com>
2021-07-28 16:06:53 -0300

    Adiciona exercício de fixação de git sobre variáveis

---
+++ fundamentos-do-desenvolvimento-web/bloco-4-introducao-a-javascript/primeiros-passos/scripts.js
@@ -0,0 +1,8 @@
+const myName = "Leandro";
+const birthCity = "Aimores-MG";
+let birthYear = 1992;
+console.log(myName, birthCity, birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "Baixo Guandu-ES"
+console.log(birthCity)
