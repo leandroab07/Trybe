668a470ba7f6d49eecb5e50c4456b642335085b0
Leandro Basilio <leandroab07@gmail.com>
2021-07-29 15:06:37 -0300

    Exercício de fixação sobre if e else

--- fundamentos-do-desenvolvimento-web/bloco-4-introducao-a-javascript/primeiros-passos/scripts.js
+++ fundamentos-do-desenvolvimento-web/bloco-4-introducao-a-javascript/primeiros-passos/scripts.js
@@ -15,3 +15,15 @@ let area = base*altura;
 let perimetro = (2*base) + (2*altura);
 
 console.log('Área: ', area, 'Perímetro: ', perimetro);
+
+let notaCandidato1 = 78;
+
+if (notaCandidato1 >= 80) {
+    console.log('Parabéns, você foi aprovado(a)!');
+}
+else if (notaCandidato1 < 80 && notaCandidato1 >= 70) {
+    console.log('Você está na nossa lista de espera');
+}
+else {
+    console.log('Você foi reprovado(a)!');
+}
