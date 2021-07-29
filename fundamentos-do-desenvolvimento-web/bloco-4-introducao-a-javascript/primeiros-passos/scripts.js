c38beee36a98b29867ad2c3e13ac5ef6c4fcab73
Leandro Basilio <leandroab07@gmail.com>
2021-07-29 02:17:01 -0300

    Exercícios de fixação

--- fundamentos-do-desenvolvimento-web/bloco-4-introducao-a-javascript/primeiros-passos/scripts.js
+++ fundamentos-do-desenvolvimento-web/bloco-4-introducao-a-javascript/primeiros-passos/scripts.js
@@ -1,8 +1,17 @@
-const myName = "Leandro";
-const birthCity = "Aimores-MG";
-let birthYear = 1992;
-console.log(myName, birthCity, birthYear);
-birthYear = 2030;
-console.log(birthYear);
-birthCity = "Baixo Guandu-ES"
-console.log(birthCity)
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId, typeof isEnrolled, typeof patientInfo, typeof patientId, typeof patientAge);
+
+let base = 5;
+let altura = 8;
+
+let area = base*altura;
+let perimetro = (2*base) + (2*altura);
+
+console.log('Área: ', area, 'Perímetro: ', perimetro);
